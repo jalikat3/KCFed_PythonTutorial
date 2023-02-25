1beac507c288b1cde659dc8a957bde8a8af168fd
jalikat3 <78003140+jalikat3@users.noreply.github.com>
2023-02-25 11:30:47 -0600

    commit

--- kcfed/public/script.js
+++ kcfed/public/script.js
@@ -1,6 +1,6 @@
-let pyodide = null;
+/* let pyodide = null;
 
-importScripts('https://cdn.jsdelivr.net/pyodide/v0.18.1/full/pyodide.js');
+window.languagePluginUrl ('https://cdn.jsdelivr.net/pyodide/v0.18.1/full/pyodide.js');
 
 const tutorialSteps = [
   {
@@ -16,15 +16,30 @@ const tutorialSteps = [
 ];
 
 window.onload = async function() {
-  await loadPyodide({indexURL : 'https://cdn.jsdelivr.net/pyodide/v0.18.1/full/'});
+  pyodide = await loadPyodide({ indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.18.1/full/' });
+  // Pyodide is now ready to use...
+  console.log(pyodide.runPython(`
+    import sys
+    sys.version
+  `));
+  let consoleElement=document.getElementById("console");
   pyodide.globals.print = function(output) {
     consoleElement.innerHTML += output + "\n";
   };
 };
 
+async function loadPyodideIfNeeded() {
+  if (pyodide === null) {
+      pyodide = await loadPyodide({ indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.18.1/full/' });
+      let consoleElement=document.getElementById("console");
+
+      pyodide.globals.print = function(output) {
+          consoleElement.innerHTML += output + "\n";
+      };
+  }
+}
 
 function checkUserInput() {
-  const userInput = document.getElementById("input").value.trim();
   const currentStep = tutorialSteps.find(step => step.expectedCode === userInput);
   if (currentStep) {
     document.getElementById("instruction").textContent = currentStep.instruction;
@@ -38,6 +53,7 @@ async function runCode() {
   await loadPyodideIfNeeded();
   await eraseConsole();
   var code = document.getElementById("input").value.trim();
+  let consoleElement=document.getElementById("console");
   console.log(code);
   var changing = document.getElementById("change");
   if (checkUserInput()) {
@@ -53,6 +69,7 @@ async function runCode() {
 }
 
 function eraseConsole() {
+  let consoleElement=document.getElementById("console");
   consoleElement.innerHTML = "";
   pyodide.runPython("");
   console.log("it worked");
@@ -60,3 +77,110 @@ function eraseConsole() {
 
 document.getElementById("instruction").textContent = tutorialSteps[0].instruction;
 document.getElementById("example_code").textContent = tutorialSteps[0].expectedCode;
+ */
+
+let pyodide = null;
+window.languagePluginUrl ('https://cdn.jsdelivr.net/pyodide/v0.18.1/full/pyodide.js');
+
+async function loadTutorialSteps() {
+  const response = await fetch('tutorial-steps.json');
+  const data = await response.json();
+  return data.tutorialSteps;
+}
+
+async function init() {
+  const tutorialSteps = await loadTutorialSteps();
+  if(tutorialSteps){
+    console.log("Steps loaded")
+  }
+}
+
+
+window.onload = async function() {
+  pyodide = await loadPyodide({ indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.18.1/full/' });
+  // Pyodide is now ready to use...
+  console.log(pyodide.runPython(`
+    import sys
+    sys.version
+  `));
+  let consoleElement=document.getElementById("console");
+  pyodide.globals.print = function(output) {
+    consoleElement.innerHTML += output + "\n";
+  };
+  await init();
+};
+
+async function loadPyodideIfNeeded() {
+  
+  if (pyodide === null) {
+      pyodide = await loadPyodide({ indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.18.1/full/' });
+      let consoleElement=document.getElementById("console");
+
+      pyodide.globals.print = function(output) {
+          consoleElement.innerHTML += output + "\n";
+      };
+  }
+}
+
+async function checkUserInput() {
+  const userInput = document.getElementById("input").value.trim();
+  const tutorialSteps = await loadTutorialSteps();
+  const currentStep = tutorialSteps.find(step => step.expectedCode === userInput);
+  if (currentStep) {
+    document.getElementById("instruction").textContent = currentStep.instruction;
+    document.getElementById("example_code").textContent = currentStep.expectedCode;
+    return true;
+  } else {
+    const currentExpectedCode = tutorialSteps.find(step => step.step === currentStepNumber).expectedCode;
+    if (userInput === currentExpectedCode) {
+      currentStepNumber++;
+      const nextStep = tutorialSteps.find(step => step.step === currentStepNumber);
+      if (nextStep) {
+        document.getElementById("instruction").textContent = nextStep.instruction;
+        document.getElementById("example_code").textContent = nextStep.expectedCode;
+        document.getElementById("change").innerHTML = "Great job! Now try the next step.";
+        document.getElementById("change").classList.add("animate");
+        return true;
+      } else {
+        document.getElementById("instruction").textContent = "Congratulations, you have completed the tutorial!";
+        document.getElementById("example_code").textContent = "";
+        document.getElementById("change").innerHTML = "Reset";
+        document.getElementById("change").classList.remove("animate");
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
+}
+
+
+
+
+async function runCode() {
+  const tutorialSteps = await loadTutorialSteps();
+  await loadPyodideIfNeeded();
+  await eraseConsole();
+  var code = document.getElementById("input").value.trim();
+  let consoleElement=document.getElementById("console");
+  console.log(code);
+  var changing = document.getElementById("instruction");
+  if (checkUserInput()) {
+    changing.innerHTML = "Great job! Now try the next step.";
+    changing.classList.add("animate");
+  } else {
+    try {
+      pyodide.runPython(code);
+    } catch (err) {
+      consoleElement.innerHTML += err.toString() + "\n";
+    }
+  }
+}
+
+function eraseConsole() {
+  let consoleElement=document.getElementById("console");
+  consoleElement.innerHTML = "";
+  pyodide.runPython("");
+  console.log("it worked");
+}
+
